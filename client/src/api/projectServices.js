a01ec29526cd59343d415d866730da143bc5d19a
sinisam92 <sinisamanojlovic1@gmail.com>
2024-10-12 22:42:08 +0200

    added project crud operations

--- client/src/api/projectServices.js
+++ client/src/api/projectServices.js
@@ -4,4 +4,28 @@ const getProjects = async () => {
   return await apiClient("projects");
 };
 
-export { getProjects };
+const getProject = async (projectId) => {
+  return await apiClient(`projects/${projectId}`);
+};
+
+const createProject = async (project) => {
+  return await apiClient("projects", {
+    method: "POST",
+    body: JSON.stringify(project),
+  });
+};
+
+const updateProject = async (project) => {
+  return await apiClient(`projects/${project.id}`, {
+    method: "PUT",
+    body: JSON.stringify(project),
+  });
+};
+
+const deleteProject = async (projectId) => {
+  return await apiClient(`projects/${projectId}`, {
+    method: "DELETE",
+  });
+};
+
+export { getProjects, getProject, createProject, updateProject, deleteProject };
