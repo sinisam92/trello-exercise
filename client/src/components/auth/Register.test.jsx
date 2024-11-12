481a82906f1d663ffd55f573a1137bedf1970325
sinisam92 <sinisamanojlovic1@gmail.com>
2024-11-12 11:24:30 +0100

    rules test #2

--- client/src/components/auth/Register.test.jsx
+++ client/src/components/auth/Register.test.jsx
@@ -20,7 +20,7 @@ describe("Register", () => {
     const passwordInput = screen.getByLabelText("Password");
     const registerButton = screen.getByRole("button", { name: /Sign up/i });
 
-    expect(usernameInput).not.toBeInTheDocument();
+    expect(usernameInput).toBeInTheDocument();
     expect(firstNameInput).toBeInTheDocument();
     expect(lastNameInput).toBeInTheDocument();
     expect(emailInput).toBeInTheDocument();
