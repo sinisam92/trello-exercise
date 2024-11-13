34fa53cc5a77efec9b5a1c769e5f4756aa16ec90
sinisam92 <sinisamanojlovic1@gmail.com>
2024-11-13 11:06:12 +0100

    actions test #3

--- client/src/components/auth/Register.jsx
+++ client/src/components/auth/Register.jsx
@@ -50,7 +50,7 @@ const Register = () => {
       };
 
       const resultAction = await dispatch(registerNewUser(newUser));
-      console.log("resultAction12344566", resultAction);
+      console.log("resultAction", resultAction);
       if (
         registerNewUser.fulfilled.match(resultAction) &&
         !resultAction.payload.error
