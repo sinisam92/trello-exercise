cefb6b0a798aa335b71b4737f5d69ac7bd4f90cd
sinisam92 <sinisamanojlovic1@gmail.com>
2024-11-22 13:35:34 +0100

    readded conflit missing files

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,4 +3,5 @@ export default {
   transform: {},
   moduleFileExtensions: ["js", "mjs", "cjs", "jsx", "json", "node"],
   testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+  setupFilesAfterEnv: ["./tests/jest.setup.js"],
 };
